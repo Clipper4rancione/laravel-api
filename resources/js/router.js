4afbebd9c26acc92d3bbcbf93c450b74a2f32ac9
Ciro Meluziis <meluziis.ciro99@gmail.com>
2023-01-30 16:59:38 +0100

    [add] routes

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+import Home from './pages/Home.vue';
+import About from './pages/About.vue';
+import Contacts from './pages/Contacts.vue';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -7,7 +10,19 @@ const router = createRouter({
         {
             path: '/',
             name: 'home',
-            component:
+            component: Home
+        },
+        {
+            path: '/chi-sono',
+            name: 'about',
+            component: About
+        },
+        {
+            path: '/contatti',
+            name: 'contacts',
+            component: Contacts
         }
     ]
-})
+});
+
+export { router };
